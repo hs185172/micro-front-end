c94d26bfaa82516aad773c02b85b51aee9b18bcc
Ghotra <hs185172@ncr.com>
2021-07-27 05:23:54 -0400

    Add route and microfrontend component for routing and using for the main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
       <p>this is our main app</p>
+      <Routes/>
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MicroFrontend from './components/MircoFrontend';
+
+const Routes = (props) => {
+
+    switch(props.route && props.currentRoute) {
+        case 'address': 
+        return address;
+    }
+}
+
+const address = () => {
+    return (
+        <MicroFrontend/>
+    )
+}
+
+export default Routes;
